aa6b371095f8c8e9b1450efd4674610a39a505a3
gustavodainez-arch <gustavo.dainez@escola.pr.gov.br>
2025-12-03 07:45:16 -0300

    bg

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const avanca = document.querySelectorAll('.btn-proximo')
+const avanca = document.querySelectorAll(".btn-proximo")
+console.log(avanca)
 
 
 avanca.forEach(button =>{
    button.addEventListener('clik',function(){
     const atual =document.querySelector('.ativo')
-    const proximoPasso = 'passo-' + getAttribute('data-proximo')
-
+    const proximoPasso = 'passo-' + this.getAttribute('data-proximo')
+    console.log(proximoPasso)
         atual.classList.remove('ativo')
         document.getElementBYId(proximoPasso).classList.add('ativo')
